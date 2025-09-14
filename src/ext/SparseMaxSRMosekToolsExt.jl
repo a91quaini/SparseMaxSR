6d427db16c7178c50ad88445f27ade9cdf2bbc04
a91quaini <alberto91quaini@gmail.com>
2025-09-14 10:49:35 +0200

    update gitignore

--- src/ext/SparseMaxSRMosekToolsExt.jl
+++ src/ext/SparseMaxSRMosekToolsExt.jl
@@ -1,4 +1,11 @@
-module SparseMaxSRMosekExt
-# Loaded only when MosekTools is installed.
+module SparseMaxSRMosekToolsExt
+
 import MosekTools
+import SparseMaxSR: set_default_optimizer!
+
+# Set MosekTools as the default optimizer for dual subproblems (QP/SOCP/QCQP).
+function __init__()
+    set_default_optimizer!(() -> MosekTools.Optimizer())
 end
+
+end # module
